085663e2eca2b9cad1035d5fd9d9ed575dcfd3dd
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2024-01-15 20:29:51 +0530

    init-stage-0

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Business Ocean <businessocean.pvt.ltd@gmail.com>
+This file is part of CLI application foo.
+*/
+package main
+
+import "github.com/businessocean/cmd"
+
+func main() {
+	cmd.Execute()
+}
