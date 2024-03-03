a34dbce48605f850c8bcb65333010972e40e45cd
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2024-03-03 15:00:34 +0530

    added script setup

--- Makefile
+++ Makefile
@@ -1,11 +1,31 @@
 run:
-	@go run main.go
+	go run ./app/main.go;
+
+tidy:
+	./scripts/go_mod_tidy.sh;
+
+cleanup:
+	./scripts/cleanup.sh;
+
+modadd:
+	./scripts/go_mod_add.sh;
+
+lint:
+	./scripts/golangci-lint.sh;
+
+go-upgrade:
+	./scripts/go_upgrade.sh;
+
+mod-run:
+	./scripts/go_run_parallel.sh;
+
+mod-sync:
+	go work sync;
+
 
 generate:
 	cd .proto;
 	buf generate proto;
 
 
-
-
-.PHONY run
+.PHONY: run tidy generate cleanup modadd lint go-upgrade mod-sync
--- becore/becore.go
+++ becore/becore.go
@@ -1,1 +1,17 @@
 package becore
+
+type BeCore interface {
+
+	// IsDev returns whether the app is in dev mode.
+	IsDev() bool
+
+	// Bootstrap takes care for initializing the application
+	// (open db connections, load settings, config setting, etc.).
+	//
+	// It will call ResetBootstrapState() if the application was already bootstrapped.
+	Bootstrap() error
+
+	// ResetBootstrapState takes care for releasing initialized app resources
+	// (eg. closing db connections).
+	ResetBootstrapState() error
+}
