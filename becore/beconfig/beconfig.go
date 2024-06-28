c844c76116a601af44da9b2bcae7920da984824e
Sourav pandit <businessocean.pvt.ltd@gmail.com>
2024-06-28 23:48:21 +0530

    setup config

--- becore/beconfig/beconfig.go
+++ becore/beconfig/beconfig.go
@@ -15,11 +15,53 @@ type Config struct {
 	MaxMultipartMemory int64  `mapstructure:"MAX_MULTIPART_MEMORY"`
 }
 
+// Config holds the configuration values
+type AppConfig struct {
+	Dgraph   DgraphConfig
+	Postgres PostgresConfig
+	JWT      JWTConfig
+	Cloud    CloudConfig
+	Common   CommonConfig
+}
+
+type DgraphConfig struct {
+	Host string
+}
+
+type PostgresConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+}
+
+type JWTConfig struct {
+	Secret string
+}
+
+type CloudConfig struct {
+	Secret string
+}
+
+type CommonConfig struct {
+	SomeOtherSecret string
+}
+
 // NewConfig returns a new instance of ServerConfig
-func NewConfig(logger belogger.BeLogger) *Config {
+func NewConfig(logger belogger.BeLogger) (*Config, error) {
 
-	viper.SetConfigFile(".env")
-
+	// viper.SetConfigFile(".env")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+	var config AppConfig
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
 	err := viper.ReadInConfig()
 	if err != nil {
 		logger.Fatal("cannot read configuration from .env file with viper ", err)
@@ -32,7 +74,7 @@ func NewConfig(logger belogger.BeLogger) *Config {
 		logger.Fatal("environment cant be loaded: ", err)
 	}
 
-	return &globalConfig
+	return &globalConfig, nil
 }
 
 var globalConfig = Config{
--- beservice/go.mod
+++ beservice/go.mod
@@ -3,15 +3,15 @@ module beservice
 go 1.22.1
 
 require (
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1
-	google.golang.org/grpc v1.63.2
-	google.golang.org/protobuf v1.34.1
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0
+	google.golang.org/grpc v1.64.0
+	google.golang.org/protobuf v1.34.2
 )
 
 require (
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.15.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240227224415-6ceb2ff114de // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240513163218-0867130af1f8 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240624140628-dc46fd24d27d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240624140628-dc46fd24d27d // indirect
 )
